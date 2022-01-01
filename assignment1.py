566425c632a86e258961b99801fb7f4db6546eab
salmanbinmehmood <86271285+salmanbinmehmood@users.noreply.github.com>
2022-01-02 04:51:30 +0500

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,39 @@
+# 1.
+
+poem='''Twinkle, twinkle, little star,
+            How I wonder what you are!
+                Up above the world so high,
+                Like a diamond in the sky.
+Twinkle, twinkle, little star,
+            How I wonder what you are!        
+'''
+
+print('Abswer 1')
+print(poem)
+
+# 2.
+import sys
+print('Abswer 2')
+print(sys.version)
+
+#3.
+import datetime
+print('Abswer 3')
+print(datetime.datetime.now())
+#4.
+
+r=int(input('Enter the radius: '))
+area=3.142*r**2
+print('Abswer 4')
+print(area)
+#5.
+name='Salman Mehmood'
+reversed_name=name[-7:]+name[-8:-7]+name[:6]
+print('Abswer 5')
+print(reversed_name)
+
+# 6.
+a=int(input('Enter first number :'))
+b=int(input('Enter second number :'))
+print('Abswer 6')
+print('addition with two numbers',a+b)
